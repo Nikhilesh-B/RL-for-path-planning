9dbb3f0170c84f4920e2efba5a688de25d5df712
Nikhilesh Belulkar <nb2953@columbia.edu>
2022-06-03 10:52:43 -0400

    Added potential functions, made basic functionality for the policy gradient algorithm including the computation of next state from the NN network

--- main/rl.py
+++ main/rl.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 from keras.layers import Dense, InputLayer, Dropout
 import keras
-from turtle import position
 from nlinkarm import NLinkArm
 from helper import visualize_spaces, animate
 import numpy as np
@@ -11,7 +10,7 @@ from scipy.integrate import dblquad
 from constants import OBSTACLES, START, GOAL, LINK_LENGTH
 
 
-class arm:
+class policyGradientAlgorithm:
     def __init__(self, gamma=0.003, horizon=4, inputLayer_dims=2, fc1_dims=256, fc2_dims=256, output_dims=2):
         self.gamma = gamma
         self.horizon = horizon
@@ -32,8 +31,8 @@ class arm:
             self.output
         ])
 
-        self.state_history = [] #predicted mean and the covariance for the continous action
-        self.action_history = [] #actual positions we moved to
+        self.state_history = [] 
+        self.action_history = [] 
         self.reward_history = [] 
 
 
@@ -41,9 +40,22 @@ class arm:
         mu = newState
         sigma = np.eye(2)
         nextAction = np.random.multivariate_normal(mean=mu, cov=sigma)
-
-
         return nextAction
+
+    def nextState(self, prevAction):
+        return self.network.predict(prevAction)
+
+    def computeReward(self, newAction):
+        reward = 0
+        for obst in OBSTACLES:
+            obstPotentialFunction = multivariate_normal(mean=obst[:-1])
+            reward -= obstPotentialFunction.pdf(newAction)
+            
+        reward += multivariate_normal(mean=GOAL)
+
+        return reward
+        
+
 
     def store_transition(self, newState, newAction, newReward):
         self.state_history.append(newState)
@@ -51,20 +63,35 @@ class arm:
         self.reward_history.append(newReward)
 
 
-
     def computeDiscountedReward(self):
-        discountedRewardCalc = np.zeros()
-        for (idx, action) in enumerate(self.action_history):
-            pass
+        discountedRewardCalc = np.zeros(len(self.state_history))
+        for k in range(0, len(self.reward_history)):
+            val = 0
+            for t in range(k, len(self.reward_history)):
+                val += self.reward_history[t]*((self.gamma)**(t-k))
+            
+            discountedRewardCalc[k] = val
+        
+        return discountedRewardCalc
 
             #we have the action that we have taken 
 
+    def computeLoss(self, action, discountedReward):
+        mu  = self.network.predict(action)
+
+        probDist = multivariate_normal(mean=mu, cov=np.eye(2))
+        newAction  = np.random.multivariate_normal(mean=mu, cov=np.eye(2))
+        pdf_value = probDist
             
 
 
 def main():
     ARM = NLinkArm(LINK_LENGTH, [0,0])
     visualize_spaces(ARM, START, OBSTACLES)
+
+
+    pgAlgo = policyGradientAlgorithm()
+
 
     roadmap = {(1.0,0.0):None,
                (0.83, 0.29):(1.0,0.0),
