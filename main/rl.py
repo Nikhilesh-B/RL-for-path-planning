5b63d0d13b5a89c57298af9527dfdbce63bf6316
Nikhilesh Belulkar <nb2953@columbia.edu>
2022-06-13 09:43:42 -0400

    simplified the code for a more relatistic situation

--- main/rl.py
+++ main/rl.py
@@ -13,44 +13,36 @@ import tensorflow as tf
 tfd=distributions
 
 class policyGradientAlgorithm():
-    def __init__(self, robot_environment, robot_arm, learning_rate=0.001, gamma=0.003, horizon=4, inputLayer_dims=2, fc1_dims=1, fc2_dims=1, output_dims=2):
-        self.gamma = gamma
+    def __init__(self, robot_environment, robot_arm, learning_rate=0.001, horizon=4, inputLayer_dims=2, fc1_dims=1, output_dims=2):
         self.horizon = horizon
         self.fc1_dims = fc1_dims
-        self.fc2_dims = fc2_dims
         self.inputLayer_dims = inputLayer_dims
         self.output_dims = output_dims
         self.learning_rate = learning_rate
         self.robot_environment = robot_environment
         self.robot_arm = robot_arm
-        self.discountedRewardHistory = None
-        self.reward_history = None
         self.action_history = None
         self.state_history = None
         self.network = None
-        self.output = None
+        self.outputLayer = None
         self.fc2 = None
         self.fc1 = None
         self.inputLayer = None
-
 
     def constructNN(self):
         self.inputLayer = InputLayer(input_shape=(1,2))
         self.fc1 = Dense(units=self.fc1_dims, activation='relu')
         self.fc2 = Dense(units=self.fc2_dims, activation='relu')
-        self.output = Dense(units=self.output_dims, activation='relu')
+        self.outputLayer = Dense(units=self.output_dims, activation='relu')
 
         self.network = keras.models.Sequential([
             self.inputLayer,
             self.fc1,
-            #self.fc2,
-            self.output
+            self.outputLayer
         ])
 
         self.state_history = [self.robot_environment.start]
         self.action_history = [self.robot_environment.start]
-        self.reward_history = [0]
-        self.discountedRewardHistory = None
 
     def getPrevAction(self):
         prevAction = tf.reshape(self.action_history[-1], [1,2])
@@ -58,42 +50,14 @@ class policyGradientAlgorithm():
 
     @staticmethod
     def getNextAction(newState):
-        #is the logPdf a function of the mu? at all or is not even?
         mu = newState[0]
         sigma = [[1, 0], [0, 1]]
         actionDist = tfd.MultivariateNormalFullCovariance(loc=mu, covariance_matrix=sigma)
         nextAction, logPdf = actionDist.experimental_sample_and_log_prob()
 
-        #the issue is that this newAction and the logProb of the draw is not at all a function of the original neural network
         logPdf = tf.norm(mu)
         return nextAction, logPdf
 
-    def detect_collision(self, arm, config, obstacles):
-        arm.update_joints(config)
-        points = arm.points
-        for k in range(len(points) - 1):
-            for circle in obstacles:
-                a_vec = tf.constant(points[k])
-                b_vec = tf.constant(points[k + 1])
-                c_vec = tf.constant([circle[0], circle[1]])
-                radius = circle[2]
-
-                line_vec = b_vec - a_vec
-                line_mag = tf.norm(line_vec)
-                circle_vec = c_vec - a_vec
-                proj = (1 / line_mag) * tf.tensordot(circle_vec, line_vec)
-
-                if proj <= 0:
-                    closest_point = a_vec
-                elif proj >= line_mag:
-                    closest_point = b_vec
-                else:
-                    closest_point = a_vec + line_vec * proj / line_mag
-
-                if tf.norm(closest_point - c_vec) <= radius:
-                    return True
-
-        return False
 
     def closeToGoal(self, newAction, threshold):
         theta0 = newAction[0]
@@ -116,13 +80,8 @@ class policyGradientAlgorithm():
         return goalPotentialFunctionValue, isCloseToGoal
 
     def getNextReward(self, newAction):
-        reward = 0
-
-        if self.detect_collision(arm=self.robot_arm, config=newAction, obstacles=self.robot_environment.obstacles):
-            reward -= 1000
-
         goalPotentialFunction, isCloseToGoal = self.closeToGoal(newAction=newAction, threshold=0.3)
-        reward += goalPotentialFunction
+        reward = goalPotentialFunction
 
         reward = tf.constant(reward)
         return reward
@@ -135,25 +94,11 @@ class policyGradientAlgorithm():
     def storeTransition(self, newState, newAction, newReward):
         self.state_history.append(newState)
         self.action_history.append(newAction)
-        self.reward_history.append(newReward)
 
     def print(self):
-        print(" XXXXXXXXXXXXXXX ")
+        print(" XXXXXXXXXXXXXXX NETWORK SUMMARY XXXXXXXXXXXXXXX ")
         self.network.summary()
-        print(" XXXXXXXXXXXXXXX ")
-
-    def computeDiscountedReward(self):
-        self.discountedRewardHistory = [0] * len(self.state_history)
-        for k in range(0, len(self.reward_history)):
-            val = 0
-            for t in range(k, len(self.reward_history)):
-                val += self.reward_history[t] * (self.gamma ** (t - k))
-
-            self.discountedRewardHistory[k] = val
-
-        self.discountedRewardHistory = tf.convert_to_tensor(self.discountedRewardHistory)
-
-        return self.discountedRewardHistory
+        print(" XXXXXXXXXXXXXXX NETWORK SUMMARY XXXXXXXXXXXXXXX ")
 
     def returnlogPDFVal(sigma, mean, sampleVal):
         pi = tf.constant(m.pi)
@@ -166,18 +111,8 @@ class policyGradientAlgorithm():
         return logPdf
 
     @staticmethod
-    def computeLogPdf(newAction, newState):
-        '''actually substitute the value and return the next value '''
-
-        #logPdf has no valid gradients that can be applied
-        #reward has all the gradients evaluate to nones.
-
-
-
-
-
-        policy_val = tf.reshape(-reward,shape=(1,1))
-        return policy_val
+    def computePolicy(newAction, newState):
+        pass
 
 
 def trainNetwork(pgAlgo, epochs, iterations, Arm):
